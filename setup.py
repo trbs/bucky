9790e1bdc23ec332bc7fd3eb8f691131787964b8
trbs <trbs@trbs.net>
2013-12-25 01:48:51 +0100

    add python 3 classifier

--- setup.py
+++ setup.py
@@ -46,6 +46,8 @@ setup(
         'Operating System :: POSIX',
         'Operating System :: Unix',
         'Programming Language :: Python',
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
         'Topic :: Internet :: Log Analysis',
         'Topic :: Utilities',
         'Topic :: System :: Networking :: Monitoring'
